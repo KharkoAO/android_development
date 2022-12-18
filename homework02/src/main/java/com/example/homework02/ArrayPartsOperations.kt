8394e2a9007c69879342d1eec4821addc205e9b9
Aliaksei Kharko <aliakseikharko@gmail.com>
2022-12-18 18:00:57 +0300

    created class 'ArrayPartsOperations'

--- homework02/src/main/java/com/example/homework02/ArrayPartsOperations.kt
+++ homework02/src/main/java/com/example/homework02/ArrayPartsOperations.kt
@@ -1,7 +1,7 @@
 package com.example.homework02
 
-!class ArrayPartsOperations {
-    fun getPartsOfArrayDivisionResult(arrayNumbers: IntArray): Double {
+class ArrayPartsOperations {
+    !fun getPartsOfArrayDivisionResult(arrayNumbers: IntArray): Double {
         val firstHalfArray = (arrayNumbers.copyOfRange(0, arrayNumbers.size / 2).sum()).toDouble()
         val secondHalfArray = (arrayNumbers.copyOfRange(arrayNumbers.size / 2, arrayNumbers.size)
             .reduce { x, y ->  x - y }).toDouble()
